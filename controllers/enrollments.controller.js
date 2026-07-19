0f38ee016c252df4ba0833b09d3e53dc4a38156f
Bushra-11 <bu2004hu@gmail.com>
2026-07-19 16:45:25 +0300

    feat: implement enrollment functionality with history and details views

--- controllers/enrollments.controller.js
+++ controllers/enrollments.controller.js
@@ -1,8 +1,31 @@
 const router = require("express").Router()
+const isSignedIn = require('../middleware/is-signed-in')
+const isParent = require('../middleware/is-parent')
+const Enrollment = require('../models/Enrollment')
 
 // View own enrollment history
-router.get('/', (req,res)=>{
-    res.render('enrollments/enrollment-history.ejs')
+router.get('/', isSignedIn, isParent, async (req,res)=>{
+    const allEnrollments = await Enrollment.find({ userId: req.session.user._id }).populate('activityId')
+    res.render('enrollments/enrollment-history.ejs', {enrollments: allEnrollments})
+})
+
+// Enroll child in activity
+router.post('/:activityId', isSignedIn, isParent, async(req,res)=>{
+    const enrollChild = await Enrollment.create({
+        childName: req.body.childName,
+        priceAtEnrollment: req.body.priceAtEnrollment,
+        status: req.body.status,
+        userId: req.session.user._id,
+        activityId: req.params.activityId
+    })
+
+    res.redirect(`/activities/${req.params.activityId}`)
+})
+
+// view enrollment details
+router.get('/:id', isSignedIn, isParent, async(req,res)=>{
+    const enrollment = await Enrollment.findById(req.params.id).populate('activityId')
+    res.render('enrollments/enrollment-details.ejs', {enrollment})
 })
 
 
